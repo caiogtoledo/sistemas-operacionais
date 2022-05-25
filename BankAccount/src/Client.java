e27633370e48c56d687118f71c41438181f0b662
caiobrunholi <caio.brunholi@hotmail.com>
2022-05-25 12:46:23 -0300

    add lock, not OK

--- BankAccount/src/Client.java
+++ BankAccount/src/Client.java
@@ -1,8 +1,15 @@
 import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Client extends Thread{
     private Account account;
     private String name;
+
+    // Variável de lock para acessar a fila compartilhada
+	private final Lock lock = new ReentrantLock();
 
     Buffer buffer;
     private int values[] = {10, 20, 50, 100};
@@ -30,11 +37,16 @@ public class Client extends Thread{
         Random random2 = new Random();
         int randomIndex = random2.nextInt(4);
 
+        
+
+        // lock.lock();
         if(choice % 2 == 0){
             account.deposit(values[randomIndex], name);
         }else{
             account.withdraw(values[randomIndex], name);
         }
+
+        // lock.unlock();
      
     }
 
@@ -43,7 +55,9 @@ public class Client extends Thread{
 		try {
 			// Tenta consumir um número inteiro
 			while (true) {
+                lock.lock();
                 execute();
+                lock.unlock();
 				// Dorme 200 ms
 				Thread.sleep(1000);
 				// Ou comente a linha anterior e
