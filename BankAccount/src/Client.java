82b279d1691d84c312a3de5cf6d74c279c86d1b1
caiobrunholi <caio.brunholi@hotmail.com>
2022-05-25 12:49:03 -0300

    add lock, not OK

--- BankAccount/src/Client.java
+++ BankAccount/src/Client.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-import java.util.concurrent.locks.Condition;
+// import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
