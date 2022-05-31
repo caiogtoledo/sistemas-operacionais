dd5ddfc5b9a38c473ec4be41360cecd9ff32d7b3
caiobrunholi <caio.brunholi@hotmail.com>
2022-05-30 23:33:14 -0300

    await implementation

--- BankAccount/src/Account.java
+++ BankAccount/src/Account.java
@@ -1,43 +1,48 @@
-// import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
 
 public class Account {
     private int balance;
     // Buffer buffer;
 
-    // Variável de lock para acessar a fila compartilhada
-	private final Lock lock = new ReentrantLock();
+    // Variável de lock para proteger as operações
+    private final Lock lock = new ReentrantLock();
+    // Variável de condição de saldo insuficiente
+    private final Condition noBalance = lock.newCondition();
 
     public Account(int balance) {
         this.balance = balance;
     }
 
-    public int getBalance(){
+    public int getBalance() {
         return balance;
     }
 
-    public void deposit(int value, String name) throws InterruptedException{
+    public void deposit(int value, String name) throws InterruptedException {
         lock.lock();
-        try{
+        try {
             this.balance += value;
-            System.out.println("Cliente: "+ name + " depositou " + value);
-            System.out.println("Conta: saldo atualizado de "+this.balance);
-        }finally{
+            System.out.println("Cliente: " + name + " depositou " + value);
+            System.out.println("Conta: saldo atualizado de " + this.balance);
+            noBalance.signalAll();
+        } finally {
             lock.unlock();
         }
     }
 
-    public void withdraw(int value, String name) throws InterruptedException{
-        if(this.balance > value){
-            // criar variavel que causa o await()
-            this.balance -= value;
-            System.out.println("Cliente: "+ name + " retirou " + value);
-            System.out.println("Conta: saldo atualizado de "+this.balance);
-        }else{
-            System.out.println("Saldo insuficiente");
-            System.out.println("Conta: saldo atualizado de "+this.balance);
+    public void withdraw(int value, String name) throws InterruptedException {
+        lock.lock();
+        try {
+            if (this.balance < value) {
+                System.out.println("Saldo insuficiente");
+                noBalance.await();
+            }
+                this.balance -= value;
+                System.out.println("Cliente: " + name + " retirou " + value);
+                System.out.println("Conta: saldo atualizado de " + this.balance);
+        } finally {
+            lock.unlock();
         }
     }
 }
